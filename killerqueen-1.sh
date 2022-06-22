d91f2d0431c11562531ce2fc44b49eac9639c6f1
Berkcan Çakar <105746490+berkcaNode@users.noreply.github.com>
2022-06-22 20:59:14 +0300

    Create killerqueen-1.sh

---
+++ killerqueen-1.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://raw.githubusercontent.com/berkcaNode/KujiraTr-Script-Kurulum/main/logo.sh 
+
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Node isminizi giriniz: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+QUICKSILVER_PORT=11
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export QUICKSILVER_CHAIN_ID=killerqueen-1" >> $HOME/.bash_profile
+echo "export QUICKSILVER_PORT=${QUICKSILVER_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Node isminiz  : \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Cüzdan isminiz: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Ağ adı        : \e[1m\e[32m$QUICKSILVER_CHAIN_ID\e[0m"
+echo -e "Your port: \e[1m\e[32m$QUICKSILVER_PORT\e[0m"
+echo '================================================='
+sleep 2
+
+echo -e "\e[1m\e[32m1. Paketler Güncelleniyor... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Bağımlılıklar Yükleniyor... \e[0m" && sleep 1
+# packages
+sudo apt install curl build-essential git wget jq make gcc tmux -y
+
+# install go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+echo -e "\e[1m\e[32m3. İkili dosyalar indiriliyor ve oluşturuluyor... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.3.0
+cd quicksilver
+make build
+sudo chmod +x ./build/quicksilverd && sudo mv ./build/quicksilverd /usr/local/bin/quicksilverd
+
+# config
+quicksilverd config chain-id $QUICKSILVER_CHAIN_ID
+quicksilverd config keyring-backend test
+quicksilverd config node tcp://localhost:${QUICKSILVER_PORT}657
+
+# init
+quicksilverd init $NODENAME --chain-id $QUICKSILVER_CHAIN_ID
+
+# download genesis and addrbook
+wget -qO $HOME/.quicksilverd/config/genesis.json "https://raw.githubusercontent.com/ingenuity-build/testnets/main/killerqueen/genesis.json"
+
+# set peers and seeds
+SEEDS="dd3460ec11f78b4a7c4336f22a356fe00805ab64@seed.killerqueen-1.quicksilver.zone:26656"
+PEERS="b281289df37c5180f9ff278be5e29964afa0c229@185.56.139.84:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.quicksilverd/config/config.toml
+
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${QUICKSILVER_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${QUICKSILVER_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${QUICKSILVER_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${QUICKSILVER_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${QUICKSILVER_PORT}660\"%" $HOME/.quicksilverd/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${QUICKSILVER_PORT}317\"%; s%^address = \":8080\"%address = \":${QUICKSILVER_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${QUICKSILVER_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${QUICKSILVER_PORT}091\"%" $HOME/.quicksilverd/config/app.toml
+
+# disable indexing
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.quicksilverd/config/config.toml
+
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.quicksilverd/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.quicksilverd/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.quicksilverd/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.quicksilverd/config/app.toml
+
+# set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0uqck\"/" $HOME/.quicksilverd/config/app.toml
+
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.quicksilverd/config/config.toml
+
+# reset
+quicksilverd tendermint unsafe-reset-all
+
+echo -e "\e[1m\e[32m4. Servis başlatılıyor... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/quicksilverd.service > /dev/null <<EOF
+[Unit]
+Description=quicksilver
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which quicksilverd) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable quicksilverd
+sudo systemctl restart quicksilverd
+
+echo '=============== KURULUM TAMAMLANDI ==================='
+echo -e 'Loglarınızı Kontrol Edin: \e[1m\e[32mjournalctl -u quicksilverd -f -o cat\e[0m'
+echo -e "Senkronizasyon Durumunu Kontrol Edin: \e[1m\e[32mcurl -s localhost:${QUICKSILVER_PORT}657/status | jq .result.sync_info\e[0m"
